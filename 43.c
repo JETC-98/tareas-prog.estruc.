019bb8b4912ab43424c0f8d27ed14adffe14617f
morogort <47389754+morogort@users.noreply.github.com>
2019-02-12 17:31:44 -0600

    ADA_4

---
+++ 43.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main () 
+{
+	int mcont,contpos,contneg;
+	float d;
+	mcont=0;
+	contpos=0;
+	contneg=0;
+	do
+	{
+		printf ("escriba un numero ");
+		scanf ("%f",&d);
+		if (d<0)
+		{
+			contneg++;
+		}
+		if (d>0)
+		{
+			contpos=contpos+1;
+		}
+		if (d==0)
+		{
+			printf ("El 0 no es negativo ni positivo introduzca otro numero \n");
+			mcont--;
+		}
+		mcont++;
+	} while(mcont<5);
+	printf ("Hubo %d positivos y %d negativos",contpos,contneg);
+}
---
+++ eje33.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+	float a;
+	int b;
+	b=0;
+	while (b<10)
+	{
+		printf ("Introduzca un numero ");
+		scanf ("%f",&a);
+		if (a>100)
+		{
+			printf ("EL numero %.2f es mayor a 100 \n",a);
+		}
+		b++;
+	}
+}

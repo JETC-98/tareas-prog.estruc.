680ce6cd2e9889b63b94d68f220cc4abe13fd46e
Abigael Arjona <47390387+AbiArjona@users.noreply.github.com>
2019-02-17 19:06:17 -0600

    Add files via upload

---
+++ ADA 5-prog estruc/ADA 5/Ejercicio 46.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/*	Ejercicio: ADA 4, Ejercicio 46.
+Descripción: E.P.  que  lea  200  números  y  que  calcule  e  imprima  cuantos son
+negativos, cuantos son positivos y cuantos fueron iguales a cero.
+Fecha: 17 Febrero 2019
+Versión: 1.0
+*/
+
+int main() {
+	
+	int cont,contp,contn,cont0;
+	float num;
+	
+	contp=0;
+	contn=0;
+	cont0=0;
+	
+	for(cont=1;cont<=200;cont++){
+		
+		printf("\nIntroduzca un número: ");
+		scanf ("%f",&num);
+		
+		if(num > 0){
+			contp++;
+		}
+		else if(num < 0){
+			contn++;
+		}
+		else{
+			cont0++;
+		}
+		
+	}
+	
+	printf("\nLa cantidad de números positivos es de %i",contp);
+	printf("\nLa cantidad de números posnegativos es de %i",contn);
+	printf("\nLa cantidad de ceros es de %i",cont0);
+	
+	return 0;
+}
+
---
+++ ADA 5-prog estruc/ADA 5/Ejercicio 47.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*	Ejercicio: ADA 4, Ejercicio 47.
+Descripción: E.P. que lea las calificaciones de 50 alumnos y que determine e
+imprima cuantos tienen calificación mayor al promedio de grupo.
+Fecha: 17 Febrero 2019
+Versión: 1.0
+*/
+
+int main() {
+	
+	int cont,contm;
+	float cal,prom,sum;
+	
+	sum = 0;
+	contm = 0;
+	
+	for(cont=1;cont<=50;cont++){
+		
+		printf("\nAlumno %i",cont);
+		
+		printf("\nIntroduzca su calificación: ");
+		scanf ("%f",&cal);
+		
+		sum += cal;
+		
+	}
+	
+	prom = (sum / 50);
+	
+	printf("           -------------");
+	
+	for(cont=1;cont<=50;cont++){
+		
+		printf("\nAlumno %i",cont);
+		
+		printf("\nIntroduzca su calificación: ");
+		scanf ("%f",&cal);
+		
+		if(cal > prom){
+			contm++;
+		}
+		
+	}
+	
+	printf("\n%i alumnos obtuvieron una calificación mayor al promedio.",contm);
+	
+	return 0;
+}
+

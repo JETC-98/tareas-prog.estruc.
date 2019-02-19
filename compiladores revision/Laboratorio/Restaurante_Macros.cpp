a8c08da871bef29af7e9c690886c474ee3ad6c26
morogort <47389754+morogort@users.noreply.github.com>
2019-02-19 13:12:25 -0600

    Add files via upload

---
+++ compiladores revision/Laboratorio/Restaurante_Macros.cpp
@@ -0,0 +1,88 @@
+/*
+II. Un restaurante ofrece hamburguesas sencillas, dobles y triples, 
+las cuales tienen un costo de $60.00, $80.00 y $100.00, respectivamente. 
+La empresa acepta tarjetas de crédito con un cargo adicional de 3% sobre 
+la compra. Los clientes pueden hacer su pedido con cualquier cantidad de 
+hamburguesas de los tipos que deseen comer.  E.P. para determinar cuánto 
+debe pagar cada uno de los N clientes que llegan al establecimiento. También,
+ al final del día, se requiere imprimir el monto total de las ventas y 
+el total de las comisiones por tarjeta de crédito. 
+Utilizar sólo estructuras repetitivas for.
+
+*/
+
+#include <stdio.h>
+#define ordenu 1
+#define ordend 2
+#define ordent 1
+#define comisionp 0.03 
+#define H1 60
+#define H2 80
+#define H3 100
+
+
+int main() {
+	
+	int clientes,pedido,cs,cd,ct,pago,total1,total2,orden,totalventas,comision,sumcom,sumtotalventas;
+	comision=0;
+	sumcom=0;
+	sumtotalventas=0;
+	cs=0;
+	cd=0;
+	ct=0;
+	
+	int i;
+	int x;
+	printf("\n Ingrese el numero de clientes que desean ordenar: ");
+	scanf("%d",&clientes);
+	
+	for (i=1;i<=clientes;i++){
+		
+		printf("\n Cliente %d :",i);
+		printf("\n Ingrese la cantidad de hamburguesas a ordenar ");
+		printf("\n :");
+		scanf("%d",&pedido);
+		printf("\n/////////////////////////////////////////");
+		printf("\n Ingrese (1)sencilla,(2)doble,(3)triple");
+		printf("\n :");
+		for(x=1;x<=pedido;x++){
+			scanf("%d",&orden);
+			if(orden==ordenu){
+				cs++;
+			}else if(orden==ordend){
+				cd++;
+			}else if(orden==ordent){
+				ct++;
+			}
+			
+		}
+		total1=((cs*H1)+(cd*H2)+(ct*H3));
+		cs=0;
+		cd=0;
+		ct=0;
+		printf("Ingrese el metodo de pago");
+		printf("(1)efectivo (2)tarjeta");
+		scanf("%d",&pago);
+		if(pago==1){
+		   total2=total1;
+			
+		}else {
+			
+		   comision=(total2*comisionp);
+		   total2=(total1+comision);
+		}
+		printf("\n La cantidad a pagar del cliente %d es: $ %d",i,total2);
+		sumcom=sumcom+comision;
+		comision=0;
+		sumtotalventas=sumtotalventas+total2;
+		
+		
+	}
+	printf("\n total de clientes: %d",clientes);
+	printf("\n total de comisiones con tarjeta: $ %d",sumcom);
+	printf("\n total de ventas: $ %d",sumtotalventas);
+	
+	
+	return 0;
+}
+
---
+++ compiladores revision/Laboratorio/Robot.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int Num_mov; 
+	int i; 
+	int w=0; 
+	int k=0;
+	float Direc;
+	int x=0;
+	int y=0;
+	
+	
+	do{
+		printf("Cuantos movimientos va an dar el robot: ");
+		scanf("%d",&Num_mov);
+		if(Num_mov>=0){
+			printf("Direccion valida\n");
+			printf("Implemente tab para avanzar");
+			k=1;
+		}else{
+			printf("No puedes ingresar negativos\n");
+			printf("Implemente tab para avanzar");
+		}
+		
+		getchar();
+		getchar();
+		system("cls");
+		
+	} while(k==0);
+	
+	
+	if(Num_mov>0){
+		
+		for(i=0; i<Num_mov; i++){
+			
+			do{
+				
+				
+				system("cls");
+				printf("\n**************************************");
+				printf("\n    6 :Aumenta coordenada x por 1\n");
+				printf("\n    4 :Reduce coordenada x por 1\n");
+				printf("\n    8 :Aumenta coordenada y por 1\n");
+				printf("\n    2 :Reduce coordenada y por 1\n");
+				printf("\n*************************************");
+				
+				printf("\nMovimiento numero: %d de %d que hace el robot\n",i+1,Num_mov);
+				
+				printf("\nDime para donde se mueve el robot: ");
+				scanf("%f",&Direc);
+				
+				if(Direc ==6){
+					x=(x+1);
+					w=1;
+					printf("Implemente tab para avanzar");
+				}else if(Direc==4){
+					x=(x-1);
+					w=1;
+					printf("Implemente tab para avanzar");
+				}else if(Direc==8){
+					y=(y+1);
+					w=1;
+					printf("Implemente tab para avanzar");
+				}else if(Direc==2){
+					y=(y-1);
+					w=1;
+					printf("Implemente tab para avanzar");
+				}else printf("Direccion no valida, implemente tab para avanzar");
+				
+				getchar();
+				getchar();
+				system("cls");
+				
+				
+			} while(w==0);
+			
+			w=0;
+			
+			
+		}
+	}else{
+		
+		printf("No hay movimiento");
+		
+	}
+	
+	
+	
+	
+	printf("\nLa cordenada del robot es: x=%d  y=%d",x , y);
+	
+	
+	
+	
+	return 0;
+}
+

30be511ac96e844bfcdd5fb03db01bac85cb91c0
morogort <47389754+morogort@users.noreply.github.com>
2019-02-20 08:05:48 -0600

    Add files via upload

---
+++ ADA_2/eje26.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	float e,t,pro,pa,total;
+	/* e=examenes, t=trabajos, pro=proyecto, pa=participacion */
+	printf ("Escriba el porcentaje de calificacion(max. 100) conjunta de los dos examenes: ");
+	scanf ("%f",&e);
+	printf ("Escriba la calificacion en tareas y trabajos: ");
+	scanf ("%f",&t);
+	printf ("Escriba la califiacion del proyecto final: ");
+	scanf ("%f",&pro);
+	printf ("Escriba la calificacion de la participacion en clase: ");
+	scanf ("%f",&pa);
+	total=(e*40.0/100)+(t*30.0/100)+(pro*20.0/100)+(pa*10.0/100);
+	/* salida */
+	printf ("El total de calificacion es: %.2f",total);
+}

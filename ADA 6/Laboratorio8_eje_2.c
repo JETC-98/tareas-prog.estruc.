62b58ca451eb853b961a807ae4483e3621a6de39
morogort <47389754+morogort@users.noreply.github.com>
2019-03-06 22:44:04 -0600

    Add files via upload

---
+++ ADA 6/Laboratorio8_eje_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int vector[20];
+	int count,suma,producto;
+	count=0;
+	suma=0;
+	producto=1;
+	
+	while (count<20)
+	{
+		printf ("\nEscriba el un numero: ");
+		scanf ("%d",&vector[count]);
+		count++;
+	}
+	count=0;
+	while  (count<20)
+	{
+		if (vector[count] % 2)
+		{
+			producto=producto*vector[count];
+		}
+		else
+		{
+			suma=suma+vector[count];
+		}
+		count++;
+	}
+	printf("\nLa suma de los elementos pares es: %d",suma);
+	printf ("\nEl producto de los elementos impares es: %d",producto);
+}

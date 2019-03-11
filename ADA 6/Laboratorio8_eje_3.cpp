d560245726736e5f0d8f7487c01bca3c58cf690a
Abigael Arjona <47390387+AbiArjona@users.noreply.github.com>
2019-03-11 05:58:10 -0600

    Add files via upload

---
+++ ADA 6/Laboratorio8_eje_3.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Practica de laboratorio 8
+/*E.P. que lea un vector de 30 elementos que sean enteros y, genere e imprima  
+un vector con los elementos negativos y otro vector con los elementos positivos.*/
+
+int i,j,p=0,n=0;
+
+int main() {
+	
+	int v[30],vp[30],vn[30];
+	
+	for(i=0;i<30;i++){
+		
+		printf("Introduzca el %i° número: ",i+1);
+		scanf("%i",&v[i]);
+		
+	}
+	system("cls");
+	
+	for(i=0;i<30;i++){
+		
+		if(v[i]>=0){
+			vp[p]=v[i];
+			p++;
+		}
+		else{
+			vn[n]=v[i];
+			n++;
+		}
+		
+	}
+	
+	printf("Vector de los elementos positivos:\n\n");
+	for(j=0;j<p;j++){
+		printf("%i\n",vp[j]);
+	}
+	
+	printf("\nVector de los elementos negativos:\n\n");
+	for(j=0;j<n;j++){
+		printf("%i\n",vn[j]);
+	}
+	
+	return 0;
+}
+

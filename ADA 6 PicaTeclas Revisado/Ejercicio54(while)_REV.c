5d3cb83e2301b249b36c27dd215165a2b40eb285
Daniel <47390373+Encalada@users.noreply.github.com>
2019-03-11 08:25:30 -0600

    Add files via upload

---
+++ ADA 6 PicaTeclas Revisado/Ejercicio54(while)_REV.c
@@ -0,0 +1,56 @@
+/*
+Programa: SumaDeVectores_while.C
+Nombre equipo: Picateclas
+Descripción:  Lee dos vectores de n elementos y genera e imprime
+un tercer vector con la suma de cada uno de los elementos de los dos
+anteriores.
+Version: 1.1
+Fecha de creación: 6/mar/2019
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(int argc, char *argv[]) {
+	int n, i=0, j=0;
+	printf("Inserte el tamaño de los vectores: ");
+	scanf("%d", &n);
+	
+	float arreglo_1[n], arreglo_2[n], arreglo_3[n], sum;
+	
+	//ENTRADA- leer primer vector
+	while(i<n){
+		printf("\nInserte el elemento %i del primer vector: ", i+1);
+		scanf("%f", &arreglo_1[i]);
+		i++;
+	}
+	
+	
+	//ENTRADA- leer segundo vector
+	while(j<n){
+		printf("\nInserte el elemento %i del segundo vector: ", j+1);
+		scanf("%f", &arreglo_2[j]);
+		j++; //Por que usar j en esta situacion si en los otros 3 ciclos se utilizó el i
+	}
+	
+	//PROCESO- Calcular la suma de los elementos de los vectores y generar tercer vector
+	i=0;
+	while(i<n){
+		sum = arreglo_1[i] + arreglo_2[i];
+		arreglo_3[i] = sum;
+		i++;
+	}
+	
+	//SALIDA- imprimir tercer vector
+	printf("\nResultados: \n");
+	i=0;
+	while(i<n){
+		printf("\n%.3f\n", arreglo_3[i]);//hubiera preferido si me decia que poscion es que imprima "Posicion %d del arreglo 3"
+		i++;
+	}
+	
+	getch();
+	return 0;
+}
+
---
+++ ADA 6 PicaTeclas Revisado/Ejercicio55(do-while)_REV.c
@@ -0,0 +1,44 @@
+/*
+Programa: VectorConNegativo.C
+Nombre equipo: Picateclas
+Descripción:  Lee un vector de n elementos, el cual se sabe tiene un
+elemento negativo y determina e imprime su posición.
+Version: 1.0
+Fecha de creación: 6/mar/2019
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(int argc, char *argv[]) {
+	int n, i=1, j=1, m=1, posicion;//no es un error pero normalmente los arreglos en c estan inicializados en 0 y no en 1
+	printf("Inserte el tamaño del vector: ");
+	scanf(" %i", &n);
+	posicion=0;
+	
+	float arreglo_1[n];
+	
+	//ENTRADA- leer vector
+	do{
+		printf("\nInserte el elemento %i del vector: ", i);
+		scanf(" %f", &arreglo_1[j]);
+		i++;
+		j++;//la variable j es un tanto innecesaria si siempre tendra el mismo valor que i
+	}while(i<=n);
+	
+	//PROCESO- Determinar elemento negativo y guardar su posición
+	do{
+		if(arreglo_1[m]<0){
+			posicion=m;//Por qué guardar la posicion y no solo imprimir el contador con su valor actual?
+		}
+		m++;//el lugar de m como otro contador se podia regresar el valor de i a 1
+	}while(m<=n);
+	
+	//SALIDA- imprimir la posición
+	printf("\n El número negativo se encuentra en la posición %i del arreglo.", posicion); 
+	
+	getch();
+	return 0;
+}
+

c9ceb2948b19fd9b3a7add0d0abcd89a940f1d91
morogort <47389754+morogort@users.noreply.github.com>
2019-03-10 23:09:23 -0600

    Add files via upload

---
+++ ADA 6/ejercicio 50.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int i=0;
+	int num[50];
+	for (i=0;i<50;i++)
+	{
+		printf("Numero %d",i);
+		scanf("%d",&num[i]);
+	}
+	
+	for (i=0;i<50;i++)
+	{
+		printf("%d",num[i]);
+	}
+	
+	return 0;
+}
---
+++ ADA 6/ejercicio 55.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) 
+{
+	
+	int i=0,n=0;
+	int num[n];
+	
+	printf ("Qué tan grande sera su vector?\n");
+	scanf ("%d",&n);
+	for (i=0;i<4;i++)
+	{
+		printf("Numero %d",i);
+		scanf("%d",&num[i]);
+	}
+	for (i=0;i<4;i++)
+	{
+		if (num[i]<0)
+		{
+			printf ("El numero negativo esta en la posicion %d",i);
+		}
+		return 0;
+	}
+}

926dee2cf18cc7e839fc77722a8ae0db0cc340c6
Abigael Arjona <47390387+AbiArjona@users.noreply.github.com>
2019-03-13 16:51:09 -0600

    Add files via upload

---
+++ ADA 7/Ejercicio 57.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*ADA 7 - Ejercicio 57
+Actividad: E.P.  que  lea  una  matriz  de  N  X  M  elementos  y  que  encuentre  e
+imprima la posición de los elementos negativos.
+Fecha: 13 de Marzo del 2019
+*/
+
+int f,c,i,j;//Contadores
+
+//Proceso
+int main() {
+	
+	
+	printf("Introduzca la cantidad de filas. ");
+	scanf ("%i",&f);//Se le pide al usuario que introduzca la cantidad de filas que tendrá la matriz
+	printf("Introduzca la cantidad de columnas. ");
+	scanf ("%i",&c);//Se le pide al usuario que introduzca la cantidad de columnas que tendrá la matriz
+	
+	int matriz[f][c];//Se inicializa la matriz con las dimensiones dadas por el usuario
+	
+	//Proceso para pedir los elementos de la matriz
+	for(i=0;i<f;i++){//Proceso de las filas
+		for(j=0;j<c;j++){//Proceso de las columnas
+			printf("\nIntroduzca el elemento de la fila %i, columna %i. ",i+1,j+1);
+			scanf("%i",&matriz[i][j]);//Se le pide al usuario los elementos de la matriz
+		}
+	}
+	
+	system("cls");
+	
+	//Proceso para obtener los elementos de la matriz que son negativos
+	for(i=0;i<f;i++){//Proceso filas
+		for(j=0;j<c;j++){//Proceso columnas
+			
+			if(matriz[i][j]<0){//Condicional para obtener los elementos negativos
+				printf("El elemento de la fila %i columna %i es negativo.",i+1,j+1);
+			}
+			
+		}
+		printf("\n");//Se imprime un salto de linea
+	}
+	
+	return 0;
+}
+
---
+++ ADA 7/Ejercicio 59.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*ADA 7 - Ejercicio 59
+Actividad: E.P. que  lea  una  matriz  de  M  X  N  elementos  y  que  cuente  e
+imprima cuantos elementos son pares y cuantos son impares.
+Fecha: 13 de Marzo del 2019
+*/
+
+int f,c,i,j,elemp=0,elemi=0;//Contadores y variables para los contadores de positivos y negativos
+
+//Proceso
+int main() {
+	
+	printf("Introduzca la cantidad de filas. ");
+	scanf ("%i",&f);//Se le pide al usuario la cantidad de filas
+	printf("Introduzca la cantidad de columnas. ");
+	scanf ("%i",&c);//Se le pide al usuario la cantidad de columnas
+	printf("\n");//Imprime salto de linea
+	
+	int matriz[f][c];//Se inicializa la matriz con las dimensiones dadas
+	
+	//Proceso para obtener los elementos de la matriz
+	for(i=0;i<f;i++){//Proceso filas
+		for(j=0;j<c;j++){//Proceso columnas
+			printf("Introduzca el elemento de la fila %i columna %i. ",i+1,j+1);
+			scanf ("%i",&matriz[i][j]);//Se le pide al usuario los elementos
+		}
+	}
+	
+	system("cls");
+	
+	//Proceso para obtener los elementos pares e impares
+	for(i=0;i<f;i++){//Proceso filas
+		for(j=0;j<c;j++){//Proceso columnas
+			
+			if(matriz[i][j] % 2 == 0 ) {//Condicional para saber si el elemento es par o impar
+				elemp++;//Si el elemento es par se aumenta en uno el contador
+			}
+			else{
+				elemi++;//Si el elemento es impar se aumenta en uno el contador
+			}
+			
+		}
+	}
+	
+	//Salida
+	printf(" %i elementos son pares y %i elementos son impares.",elemp,elemi);//Se imprime la cantidad de elementos par e impares
+	
+	return 0;
+}
+

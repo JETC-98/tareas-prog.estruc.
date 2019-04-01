82c8e9a1076a1ffb514453d398d749fe44400dcd
Daniel <47390373+Encalada@users.noreply.github.com>
2019-04-01 07:49:21 -0600

    Add files via upload

---
+++ ADA 9/70.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int i;
+	float *arreglo=NULL;
+	arreglo=realloc(NULL,sizeof(float));
+	arreglo[10];
+		for(i=0;i<10;i++){
+		scanf ("%f",&arreglo[i]);
+	}
+		
+		arreglo=realloc(NULL,sizeof(float*)*2);
+		
+		for(i=0;i<20;i++){
+			printf ("\n%.2f",arreglo[i]);
+		}
+		
+	return 0;
+}
+
